bbda2060a598871fee4cb2cd3ba2c941eb5065f6
MaksimBururi <m.lubitkuy@gmail.com>
2025-02-19 12:41:25 +0300

    Deploy to GitHub Pages

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/S4SInstrument/", // Укажите имя вашего репозитория
-  plugins: [react()],
-});
